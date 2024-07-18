a639745568a93ea0e61fadebfcf3531052f66b6d
Wymhar <wymhar@gmail.com>
2024-07-18 21:38:29 +0200

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -13,7 +13,7 @@ const exp = [
   },
   {
     title: "Comunity Clinic Nursing",
-    cardImage: "assets/images/experience-page/gsoc.png",
+    cardImage: "assets/images/experience-page/Paarl Prov Hosp.png",
     place: "Klapmuts Clinic",
     time: "(Feb 2023 - Jul 2023)",
     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
